c01b5abdc54e9d06cb5ad560739ce1a7947debff
huyendt-kidsplaza <huyen.dt@kidsplaza.vn>
2024-06-21 13:47:44 +0700

    Install prettier, lint-staged && husky

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
@@ -11,18 +11,23 @@ const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware to handle data from submit form
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //http logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -30,7 +35,7 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Route init
 route(app);
 
-//127.0.0.1 
+//127.0.0.1
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});

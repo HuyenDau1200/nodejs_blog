6e56f2a4ed8b68ead894e52f4513f466ce8d79a0
huyendt-kidsplaza <huyen.dt@kidsplaza.vn>
2024-06-21 14:05:03 +0700

    Run > nodejs_blog@1.0.0 beautiful /var/www/html/nodejs/nodejs_blog > lint-staged
    [STARTED] Preparing lint-staged...
    [SUCCESS] Preparing lint-staged...
    [STARTED] Running tasks for staged files...
    [STARTED] package.json — 3 files
    [STARTED] src/**/*.{js,json,scss} — 3 files
    [STARTED] prettier --single-quote --trailing-comma all --tab-width 4 --write
    [SUCCESS] prettier --single-quote --trailing-comma all --tab-width 4 --write
    [SUCCESS] src/**/*.{js,json,scss} — 3 files
    [SUCCESS] package.json — 3 files
    [SUCCESS] Running tasks for staged files...
    [STARTED] Applying modifications from tasks...
    [SUCCESS] Applying modifications from tasks...
    [STARTED] Cleaning up temporary files...
    [SUCCESS] Cleaning up temporary files... to formater code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const {engine} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 const app = express();
 const port = 3000;
@@ -11,26 +11,31 @@ const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware to handle data from submit form
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //http logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-      // Route inits
+// Route inits
 route(app);
 
-//127.0.0.1 
-        app.listen(port, () => {
-          console.log(`Example app listening on port ${port}`)
-        })
+//127.0.0.1
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
